a4694dfb25740dc59ab7365f09216ef03c043daa
awageki <ikeo.hogehoge@gmail.com>
2022-09-12 23:43:37 +0900

    実行時間のわずかなズレでテスト失敗

--- lib/timer_base.rb
+++ lib/timer_base.rb
@@ -1,8 +1,9 @@
 class TimerBase
   def initialize(minutes:)
+    @end_time = Time.now + minutes * 60
   end
   
   def run
-        
+    @end_time
   end
 end
--- test/timer_base_test.rb
+++ test/timer_base_test.rb
@@ -3,6 +3,7 @@ require_relative '../lib/timer_base'
 
 class TimerBaseTest < Minitest::Test
   def test_timer_base
-    assert TimerBase
+    timer_base = TimerBase.new(minutes:15)
+    assert_equal Time.now + 15 * 60, timer_base.run
   end
 end
